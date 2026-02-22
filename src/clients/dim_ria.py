bfe6503824f24f076df5c70f82a54c313f7731f6
no1sebomb <noisebombch@gmail.com>
2026-02-22 16:47:00 +0200

    WIP on Dim.Ria get_latest_advertisements()

--- src/clients/dim_ria.py
+++ src/clients/dim_ria.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from math import ceil
+from datetime import datetime
 from inspect import signature
 
 from .base import BaseClient
@@ -19,7 +21,11 @@ class DimRiaClient(BaseClient):
     # Overload required attributes
     logger = dim_ria_logger
     config = config.dim_ria
+
+    # URLs
     api_url = "https://developers.ria.com/dom"
+    cdn_url = "https://cdn.riastatic.com/photos/"  # For formatting image URLs
+    public_url = "https://dom.ria.com/uk/"  # For formatting advertisement URLs
 
     @classmethod
     def _init_params(cls) -> dict:
@@ -82,7 +88,6 @@ class DimRiaClient(BaseClient):
             url=f"/info/{advertisement_id}",
         )
 
-    async def get_latest_advertisements(self) -> list[Advertisement]:
     @classmethod
     def _to_advertisement(cls, data: dict) -> Advertisement:
         """
@@ -112,8 +117,280 @@ class DimRiaClient(BaseClient):
             # Images
             photo_url=(cls.cdn_url + data["main_photo"].replace(".jpg", "xl.jpg")) if data.get("main_photo") else None,
         )
+
+    async def _find_published_after(
+            self,
+            advertisement_ids: list[int],
+            after_date: datetime,
+    ) -> tuple[list[int], dict[int, dict]]:
         """
-        Get latest advertisements from the Dim.Ria.
+        Find the IDs of advertisements published after the specified date using binary search.
+        Also, returns a dictionary with the data of the checked advertisements
+        to avoid making additional requests for them later.
         """
 
-        pass
+        async def _get_publishing_date(advertisement_id: int) -> datetime:
+            """
+            Helper function to get the publishing date of an advertisement by its ID.
+            """
+
+            data = await self.get_advertisement(advertisement_id)
+            checked[advertisement_id] = data  # Save data to checked dict
+            return datetime.fromtimestamp(data["publishing_date_ts"])
+
+        self.logger.debug(
+            f"Binary searching for advertisements published after "
+            f"{after_date} among {len(advertisement_ids)} IDs"
+        )
+        checked: dict[int, dict] = {}
+
+        if await _get_publishing_date(advertisement_ids[0]) < after_date:
+            # First ID is posted before => no matches
+            return [], checked
+
+        elif await _get_publishing_date(advertisement_ids[-1]) > after_date:
+            # Latest ID is posted after => all matches
+            return advertisement_ids, checked
+
+        # Binary search for the first ID that is posted before the specified date
+        left, right = 0, len(advertisement_ids) - 1
+
+        while left < right:
+            # Get middle index
+            mid = (left + right) // 2
+
+            if await _get_publishing_date(advertisement_ids[mid]) > after_date:
+                # Posted later in list => need to check the right half
+                left = mid + 1
+
+            else:
+                # Posted earlier in list => need to check the left half
+                right = mid
+
+        # All IDs before the first posted earlier than the specified date
+        return advertisement_ids[:right], checked
+
+    async def get_latest_advertisements(self, after_date: datetime | None = None) -> list[Advertisement]:
+        """
+        Get latest advertisements from the Dim.Ria.
+        Using data files, check for already processed advertisements, and return only new ones.
+        After processing, saves the IDs of the new advertisements to the data file.
+        If ``after_date`` is provided, only advertisements published after the specified date will be returned.
+        """
+
+        self.logger.info(f"Getting latest advertisements from Dim.Ria")
+
+        # Prepare variables
+        advertisement_ids: list[int] = []
+        checked_ids: set[int] = set()
+        advertisements: list[Advertisement] = []
+        existing_ids: set[int] = self.data.get("existing_ids") or set()
+        self.data.setdefault("existing_ids", set())
+
+        # Prepare parameters for searching
+        kwargs = {
+            "city_id": self.config.city_ids,
+            "sort": "pub_d",  # Publication date (descending)
+            "category": 1,  # Apartments
+            "realty_type": 2,  # Apartments
+            "operation": 1,  # Sale
+            "characteristics": {
+                1437: 1436,  # Only owner
+            },
+        }
+
+        if existing_ids:
+            # Process advertisements only before any of existing IDs
+            self.logger.info(f"Using {len(existing_ids)} existing IDs to filter advertisements")
+
+        elif after_date:
+            # Process advertisements only after the specified date
+            self.logger.info(f"Filtering out advertisements published after '{after_date}'")
+
+        else:
+            # Get all advertisements
+            self.logger.info("No existing IDs or 'after_date' provided, getting all advertisements")
+
+        # Load pages
+        page = 0
+        total_pages: int | None = None
+
+        while (page := page + 1) <= (total_pages or 1):
+            # Load each page
+            self.logger.info(f"Loading page ({page}/{total_pages or '?'}) of advertisements")
+            data = await self.search_advertisements(page=page - 1, **kwargs)
+
+            if ids := data.get("items"):
+                # Process IDs based on parameters
+                if existing_ids:
+                    for advertisement_id in ids:
+                        if advertisement_id in existing_ids:
+                            # Found an existing ID, stop loading more pages
+                            self.logger.info(
+                                f"Found an existing ID={advertisement_id} on page {page}, "
+                                f"stopping loading more pages",
+                            )
+                            break
+
+                        advertisement_ids.append(advertisement_id)
+
+                elif after_date:
+                    # Add only IDs posted after the specified date to the list
+                    after_ids, checked = await self._find_published_after(
+                        advertisement_ids=ids,
+                        after_date=after_date,
+                    )
+                    advertisement_ids.extend(after_ids)
+
+                    for checked_id, advertisement in checked.items():
+                        if checked_id in advertisement_ids:
+                            # Save checked advertisements to the list (filter out unmatched ones)
+                            advertisements.append(self._to_advertisement(advertisement))
+                            checked_ids.add(checked_id)
+
+                    if len(after_ids) < len(ids):
+                        # No more on next pages, stop loading more pages
+                        self.logger.info(
+                            f"Found an ID posted before '{after_date}' on page {page}, "
+                            f"stopping loading more pages",
+                        )
+                        break
+
+                else:
+                    # Add IDs to the list
+                    advertisement_ids.extend(ids)
+
+            if total_count := data.get("count"):
+                # Calculate total pages
+                total_pages = ceil(total_count / 100)  # 100 items per page
+        else:
+            # Finished loading pages
+            self.logger.info(f"Found {len(advertisement_ids)} advertisement IDs, no more pages to load")
+
+        # if existing_ids := self.data["existing_ids"]:  # type: set[int]
+        #     # Process advertisements only before any of existing IDs
+        #     self.logger.info(f"Using {len(existing_ids)} existing IDs to filter advertisements")
+        #     page = 0
+        #     total_pages: int | None = None
+        #
+        #     while (page := page + 1) <= (total_pages or 1):
+        #         # Load each page
+        #         self.logger.info(f"Loading page ({page}/{total_pages or '?'}) of advertisements")
+        #         data = await self.search_advertisements(page=page - 1, **kwargs)
+        #
+        #         if ids := data.get("items"):
+        #             for advertisement_id in ids:
+        #                 if advertisement_id in existing_ids:
+        #                     # Found an existing ID, stop loading more pages
+        #                     self.logger.info(
+        #                         f"Found an existing ID={advertisement_id} on page {page}, "
+        #                         f"stopping loading more pages",
+        #                     )
+        #                     break
+        #
+        #                 advertisement_ids.append(advertisement_id)
+        #
+        #         if total_count := data.get("count"):
+        #             # Calculate total pages
+        #             total_pages = ceil(total_count / 100)  # 100 items per page
+        #
+        #     else:
+        #         # Finished loading pages
+        #         self.logger.info(f"Found {len(advertisement_ids)} advertisement IDs, no more pages to load")
+        #
+        # elif after_date:
+        #     # Process advertisements only after the specified date
+        #     self.logger.info(f"Filtering out advertisements published after {after_date}")
+        #     page = 0
+        #     total_pages: int | None = None
+        #
+        #     while (page := page + 1) <= (total_pages or 1):
+        #         # Load each page
+        #         self.logger.info(f"Loading page ({page}/{total_pages or '?'}) of advertisements")
+        #         data = await self.search_advertisements(page=page - 1, **kwargs)
+        #
+        #         if ids := data.get("items"):
+        #             # Add only IDs posted after the specified date to the list
+        #             after_ids = await self._find_published_after(advertisement_ids=ids, after_date=after_date)
+        #             advertisement_ids.extend(after_ids)
+        #
+        #             if len(after_ids) < len(ids):
+        #                 # No more on next pages, stop loading more pages
+        #                 self.logger.info(
+        #                     f"Found an ID posted before {after_date} on page {page}, "
+        #                     f"stopping loading more pages",
+        #                 )
+        #                 break
+        #
+        #         if total_count := data.get("count"):
+        #             # Calculate total pages
+        #             total_pages = ceil(total_count / 100)  # 100 items per page
+        #
+        #     else:
+        #         # Finished loading pages
+        #         self.logger.info(f"Found {len(advertisement_ids)} advertisement IDs, no more pages to load")
+        #
+        # else:
+        #     # Get all advertisements
+        #     self.logger.info("No existing IDs or 'after_date' provided, getting all advertisements")
+        #     page = 0
+        #     total_pages: int | None = None
+        #
+        #     while (page := page + 1) <= (total_pages or 1):
+        #         # Load each page
+        #         self.logger.info(f"Loading page ({page}/{total_pages or '?'}) of advertisements")
+        #         data = await self.search_advertisements(page=page - 1, **kwargs)
+        #
+        #         if ids := data.get("items"):
+        #             # Add IDs to the list
+        #             advertisement_ids.extend(ids)
+        #
+        #         if total_count := data.get("count"):
+        #             # Calculate total pages
+        #             total_pages = ceil(total_count / 100)  # 100 items per page
+        #
+        #     else:
+        #         # Finished loading pages
+        #         self.logger.info(f"Found {len(advertisement_ids)} advertisement IDs, no more pages to load")
+
+        advertisement_ids = advertisement_ids[:20]  # TODO: Remove
+        self.logger.info(f"Loading info for {len(advertisement_ids)} advertisement(s)")
+
+        for index, advertisement_id in enumerate(advertisement_ids, start=1):
+            if advertisement_id not in checked_ids:
+                # Load each advertisement by ID
+                self.logger.info(
+                    f"({index}/{len(advertisement_ids)}) "
+                    f"Loading advertisement ID={advertisement_id}"
+                )
+
+                try:
+                    # Load advertisement data
+                    data = await self.get_advertisement(advertisement_id)
+                    advertisement = self._to_advertisement(data)
+
+                except (ValueError, Exception):
+                    # Failed
+                    self.logger.error(
+                        f"({index}/{len(advertisement_ids)}) "
+                        f"Failed to load data for advertisement ID={advertisement_id}",
+                        exc_info=True,
+                    )
+
+                else:
+                    # Success
+                    advertisements.append(advertisement)
+
+            else:
+                # Skip already checked ID
+                self.logger.info(
+                    f"({index}/{len(advertisement_ids)}) "
+                    f"Advertisement ID={advertisement_id} already processed, skipping",
+                )
+
+            # Save ID to data
+            self.data["existing_ids"].add(advertisement_id)
+
+        self.logger.info(f"Successfully collected {len(advertisements)} advertisements")
+
+        return advertisements
